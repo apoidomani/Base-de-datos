ab57ea19cbca9db0518d09e8c32a94ebd68179cf
apoidomani <81364419+apoidomani@users.noreply.github.com>
2021-06-24 16:37:24 -0300

    Add files via upload

---
+++ 22-06 Poidomani.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE EXAMEN;
+USE EXAMEN;
+CREATE TABLE ESTUDIANTES
+(idest int  not null primary key,
+nombre varchar(12) not null,
+apellido varchar(12) not null,
+nota int not null,
+curso varchar(40) not null);
